41316db645585410a276a069ce8cab9868c819db
czyner zarate <czyner7@gmail.com>
2024-02-19 12:33:07 +0800

    Creating Laravel and Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/About_Me', function (){
+    return view('About_Me');
+});
+
+Route::get('/Skills', function (){
+    return view('Skills');
+});
+
+Route::get('/Hobbies', function (){
+    return view('Hobbies');
+});
